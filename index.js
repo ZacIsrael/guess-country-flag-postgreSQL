d296e4583b640db4a2069592aace054ca0792a2a
Zacchaeus Israel <zacchaeusisrael@Dickernss-MacBook-Pro.local>
2025-04-23 09:49:31 -0400

    Connected to the 'flags' table from the 'world' database in postgreSQL in order to display the flag so the end user can guess the name that it belongs to.

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 import express from "express";
 import bodyParser from "body-parser";
 
+// pg package allows us to interact with our database in postgreSQL
+import pg from "pg";
+
+// allows us to access our passwords and other sensitive variables from the .env file
+import dotenv from 'dotenv';
+dotenv.config();
+
 const app = express();
 const port = 3000;
 
@@ -11,6 +18,36 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 let currentQuestion = {};
+
+// stores the data to check if the user's answer is correct
+let quiz = [
+  { name: "", flag: ""}
+]
+
+const db = new pg.Client({
+  user: process.env.PG_USERNAME,
+  host: "localhost",
+  // access the "world" database in postgreSQL
+  database: "world",
+  password: process.env.PG_PASSWORD,
+  port: 5432
+});
+
+// connect to the world database
+db.connect();
+
+// retrieve all of the rows/entries from the "flags" table in the "world" database
+db.query("SELECT * FROM flags", (err, res) => {
+  if(err){
+    // an error occured
+    console.error("Error executing query: ", err.stack);
+  } else {
+    // quiz is now the rows from the table
+    quiz = res.rows;
+  }
+  // close the connection to the database
+  db.end();
+});
 
 // GET home page
 app.get("/", (req, res) => {
@@ -24,7 +61,7 @@ app.get("/", (req, res) => {
 app.post("/submit", (req, res) => {
   let answer = req.body.answer.trim();
   let isCorrect = false;
-  if (currentQuestion.capital.toLowerCase() === answer.toLowerCase()) {
+  if (currentQuestion.name.toLowerCase() === answer.toLowerCase()) {
     totalCorrect++;
     console.log(totalCorrect);
     isCorrect = true;
